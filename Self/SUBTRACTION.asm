59612ec2a994a773d064f2449391e114910b4dbc
Md. Thaiabul Alam Chowdhury <123104873+alam410@users.noreply.github.com>
2024-11-02 23:00:22 +0600

    Add files via upload

---
+++ Self/SUBTRACTION.asm
@@ -0,0 +1,58 @@
+;Write the following assembly program to take input 2 digit and print the subtraction value. 
+;Sample input/output screen:
+;Enter first digit: 3
+;Enter second digit: 2
+;The result is: 1
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+NEWL DB 10,13,'$'
+MSG DB "Enter the first digit: $"
+MSG2 DB "Enter the second digit: $"  
+RES DB "The result is: $"
+
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AH,9
+    LEA DX,MSG
+    INT 21H
+    
+    MOV AH,1
+    INT 21H
+    MOV BL,AL
+    
+    
+    MOV AH,9
+    LEA DX,NEWL
+    INT 21H
+    
+    LEA DX,MSG2
+    INT 21H
+    
+    MOV AH,1
+    INT 21H
+    MOV BH,AL
+    
+    
+    MOV AH,9
+    LEA DX,NEWL
+    INT 21H
+    
+    LEA DX,RES
+    INT 21H
+    
+    SUB BL,BH
+   
+    
+    MOV AH,2
+    MOV DL,BL
+    ADD DL,'0'
+    INT 21H
+       
+    
+    MAIN ENDP
+END MAIN
